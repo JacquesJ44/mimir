85b75e5b99d219f882b0528ec1b36f17ee8b7ee1
JacquesJ44 <jacquesj44@gmail.com>
2025-07-10 19:35:28 +0200

    fix image file paths

--- mimir-fe/src/Navbar.js
+++ mimir-fe/src/Navbar.js
@@ -52,7 +52,7 @@ const Navbar = ({ token, setToken, message, setMessage }) => {
     return (
         <div className="navbar shadow-2xl bg-base-200 roundedborders">
         <div className="flex-1">
-            <img src="/aesirblue.png" className="App-logo" alt="logo" />
+            <img src="/mimir/aesirblue.png" className="App-logo" alt="logo" />
         </div>
 
         <div className="flex-none">
