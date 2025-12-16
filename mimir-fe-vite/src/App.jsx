4c0afc222438f868a9341f828906d6b40890b177
JacquesJ44 <jacquesj44@gmail.com>
2025-12-16 11:50:44 +0200

    migrate from CRA to Vite, clean up CRA files 161225

--- mimir-fe-vite/src/App.jsx
+++ mimir-fe-vite/src/App.jsx
@@ -2,22 +2,22 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css';
-import PrivateRoute from './PrivateRoute';
-import Login from './Login';
-import ForgotPassword from './ForgotPassword';
-import ResetPassword from './ResetPassword';
-import Navbar from './Navbar';
-import Sites from './Sites';
-import Circuits from './Circuits';
-import AddCircuit from './AddCircuit';
-import AddSite from './AddSite';
-import ViewSite from './ViewSite';
-import ViewCircuit from './ViewCircuit';
-import UpdateCircuit from './UpdateCircuit';
-import Register from './Register';
-import Dashboard from './Dashboard';
-import LogsPage from './Logs';
-import Commission from './Commission';
+import PrivateRoute from './PrivateRoute.jsx';
+import Login from './Login.jsx';
+import ForgotPassword from './ForgotPassword.jsx';
+import ResetPassword from './ResetPassword.jsx';
+import Navbar from './Navbar.jsx';
+import Sites from './Sites.jsx';
+import Circuits from './Circuits.jsx';
+import AddCircuit from './AddCircuit.jsx';
+import AddSite from './AddSite.jsx';
+import ViewSite from './ViewSite.jsx';
+import ViewCircuit from './ViewCircuit.jsx';
+import UpdateCircuit from './UpdateCircuit.jsx';
+import Register from './Register.jsx';
+import Dashboard from './Dashboard.jsx';
+import LogsPage from './Logs.jsx';
+import Commission from './Commission.jsx';
 
 function App() {
 
@@ -29,7 +29,7 @@ function App() {
       <div className="min-h-screen flex flex-col bg-base-200">
         <Navbar token={token} setToken={setToken} message={message} setMessage={setMessage}/>
         
-        <div className='flex-grow flex items-center justify-center'>
+        <div className='grow flex items-center justify-center'>
           <Routes>
             <Route path='/' element={ token ? <Navigate to="/circuits" /> : <Navigate to="/login" />} />
             <Route path="/login" element={<Login setToken={setToken} />} />
